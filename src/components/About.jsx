5f664cce67def32f69b3df43410ea1bb8babd0e6
ApolloSolo <matt1teixeira@gmail.com>
2022-05-29 11:41:13 -0400

    About and skills section

---
+++ src/components/About.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-[rgb(31,40,51)] text-gray-300"
+    >
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">About</p>
+          </div>
+          <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold">
+            <p>Hi, I'm Matt. Nice to meet you. Please take a look around.</p>
+          </div>
+          <div>
+            <p>
+              I am passionate about creating excellent software that improves
+              the lives of those around me. I specialize in developng tools for
+              niche processes and applications for individuals and
+              small-businesses. With each project, my aim is to provide a
+              solution for the problem at hand. What could your team accomplish
+              with a developer at your side?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Skills.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import HTML from "../assets/html.png";
+import CSS from "../assets/css.png";
+import JavaScript from "../assets/javascript.png";
+import ReactImg from "../assets/react.png";
+import Node from "../assets/node.png";
+import Mongo from "../assets/mongo.png";
+
+const Skills = () => {
+  return (
+    <div name="skills" className="w-full h-screen bg-[rgb(31,40,51)] text-gray-300">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600">Experience</p>
+          <p className="py-4">These are the technologies I have worked with</p>
+          <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8">
+            <div className="shadow-md shadow-[rgb(15,15,15)] hover:scale-110 duration-500">
+              <img className="w-20 mt-2 mx-auto" src={HTML} alt="HTML icon" />
+              <p className="my-4">HTML</p>
+            </div>
+            <div className="shadow-md shadow-[rgb(15,15,15)] hover:scale-110 duration-500">
+              <img className="w-20 mt-2 mx-auto" src={CSS} alt="CSS icon" />
+              <p className="my-4">CSS</p>
+            </div>
+            <div className="shadow-md shadow-[rgb(15,15,15)] hover:scale-110 duration-500">
+              <img className="w-20 mt-2 mx-auto" src={JavaScript} alt="JavaScript icon" />
+              <p className="my-4">JavaScript</p>
+            </div>
+            <div className="shadow-md shadow-[rgb(15,15,15)] hover:scale-110 duration-500">
+              <img className="w-20 mt-2 mx-auto" src={ReactImg} alt="ReactImg icon" />
+              <p className="my-4">ReactImg</p>
+            </div>
+            <div className="shadow-md shadow-[rgb(15,15,15)] hover:scale-110 duration-500">
+              <img className="w-20 mt-2 mx-auto" src={Node} alt="Node icon" />
+              <p className="my-4">Node</p>
+            </div>
+            <div className="shadow-md shadow-[rgb(15,15,15)] hover:scale-110 duration-500">
+              <img className="w-20 mt-2 mx-auto" src={Mongo} alt="Mongo icon" />
+              <p className="my-4">Mongo</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
